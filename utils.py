0b440a21c6ec51d68af699b41678f8d553f62e2d
mikhail <mizolotu@jyu.fi>
2021-01-06 12:14:51 +0200

    6.1.2020

--- utils.py
+++ utils.py
@@ -544,6 +544,7 @@ class Session():
                 self._ack(p)
             if p.haslayer(TCP) and p[TCP].dport == self.sport and p[TCP].flags & 0x01 == 0x01:  # FIN
                 print('received fin')
+                print(self.connected)
                 self._ack_rclose()
         s.close()
         self._ackThread = None
